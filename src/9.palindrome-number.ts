da69a9fedb44ffefa94ee827e5f2c17ddfb7aaf5
ma_c <ma_c@worksap.co.jp>
2020-11-03 20:39:31 +0800

    easy 9

---
+++ src/9.palindrome-number.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=9 lang=typescript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+export function isPalindrome(x: number): boolean {
+
+  if( x < 0 ) return false;
+  if( x === 0 ) return true;
+  if( x % 10 === 0 ) return false;
+
+  let tail = 0;
+
+  while( tail < x ){
+    const rem = x % 10;
+    tail = 10*tail + rem;
+
+    if( tail === x ) return true;
+
+    x = x / 10 | 0;
+    if( tail === x) return true;
+  }
+
+  return false;
+}
+// @lc code=end
+
