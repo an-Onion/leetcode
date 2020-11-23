863448f8bd307a16a6b4f4460fa17f04def6b82c
ma_c <ma_c@worksap.co.jp>
2020-11-23 16:59:08 +0800

    20 valid parentheses

---
+++ src/20.valid-parentheses.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=20 lang=typescript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+export function isValid(s: string): boolean {
+  const stack = [];
+
+  const pairs: Record<string, string> = Object.freeze({'(': ')', '[': ']', '{': '}'});
+
+  for(const c of s){
+    if( ['(', '[', '{'].includes(c) ){
+      stack.push(c);
+      continue;
+    }
+    const left = pairs[stack.pop()];
+
+    if( left !== c ) return false;
+  }
+
+  return stack.length === 0;
+}
+// @lc code=end
+
