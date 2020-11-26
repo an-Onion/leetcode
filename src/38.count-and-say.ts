f41695bc11e2c823dd88c4a4d244bed3ec342797
ma_c <ma_c@worksap.co.jp>
2020-11-26 19:59:07 +0800

    count and say

---
+++ src/38.count-and-say.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=38 lang=typescript
+ *
+ * [38] Count and Say
+ */
+
+// @lc code=start
+export function countAndSay(n: number): string {
+
+  if( n === 1 ) return '1';
+
+  const nMinus1: string = countAndSay(n-1);
+  let res = '', pre: string = nMinus1[0], count = 0;
+
+  for(const c of  nMinus1+'#') {
+    if( c === pre )  count += 1;
+    else {
+      res += count+pre;
+      pre = c, count = 1;
+    }
+  }
+  return res;
+}
+// @lc code=end
+
---
+++ test/38.count-and-say.test.ts
@@ -0,0 +1,11 @@
+import {countAndSay} from '../src/38.count-and-say';
+
+describe('Count and Say', () => {
+  it('1', () => {
+    expect(countAndSay(1)).toBe('1');
+  });
+
+  it('4', () => {
+    expect(countAndSay(4)).toBe('1211');
+  });
+});
