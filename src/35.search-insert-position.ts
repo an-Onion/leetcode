ea7a711f055e8f9fd6ebafff26b903a3782ecc38
ma_c <ma_c@worksap.co.jp>
2020-11-26 16:19:08 +0800

    35 serach inserted position

---
+++ src/35.search-insert-position.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=35 lang=typescript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+export function searchInsert(nums: number[], target: number): number {
+
+  let l = 0, u = nums.length - 1;
+
+  while( l <= u ) {
+    const m = ( l + u ) / 2 | 0;
+
+    if( nums[m] < target ) l = m + 1;
+    else u = m -1;
+  }
+  return l;
+}
+// @lc code=end
+
