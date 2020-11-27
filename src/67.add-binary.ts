954db955fe97704da4d9cd06009c836d3dc90157
ma_c <ma_c@worksap.co.jp>
2020-11-27 18:42:04 +0800

    67 add binary

---
+++ src/67.add-binary.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=67 lang=typescript
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+export function addBinary(a: string, b: string): string {
+
+  const binary = (c: string | undefined): number => parseInt(c || '0');
+
+  const arrA = a.split('').reverse(), arrB = b.split('').reverse();
+  const res: number[] = [];
+  let carry = 0;
+
+  for(let i = 0; i < arrA.length || i < arrB.length; ++i){
+    const sum = binary(arrA[i]) + binary(arrB[i]) + carry;
+    res.push( sum & 1 );
+    carry = sum >> 1;
+  }
+
+  if( carry ) res.push (1);
+
+  return res.reverse().join('');
+}
+// @lc code=end
+
---
+++ test/67.add-binary.test.ts
@@ -0,0 +1,11 @@
+import { addBinary } from '../src/67.add-binary';
+
+describe('Add Binary', () => {
+  it('11 + 1', () => {
+    expect(addBinary('11', '1')).toBe('100');
+  });
+
+  it('1010 + 1011', () => {
+    expect(addBinary('1010', '1011')).toBe('10101');
+  });
+});
