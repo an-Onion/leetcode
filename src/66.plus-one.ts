28356204eac293ac3e34699e9ab2159b9c1ecbed
ma_c <ma_c@worksap.co.jp>
2020-11-27 16:37:53 +0800

    plus one

---
+++ src/66.plus-one.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=66 lang=typescript
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+export function plusOne(digits: number[]): number[] {
+  digits.reverse();
+  let carry = 1;
+  const res = [];
+  for(let i = 0; i < digits.length; i++){
+    const sum = digits[i] + carry;
+    res.push(sum % 10);
+    carry = sum / 10 | 0;
+  }
+  if( carry ) res.push(carry);
+  return res.reverse();
+}
+// @lc code=end
+
