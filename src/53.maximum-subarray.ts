47559e06bf2c7447f0577a1f3a2f822f0938d2ad
ma_c <ma_c@worksap.co.jp>
2020-11-27 12:45:21 +0800

    53 maximum sub array

---
+++ src/53.maximum-subarray.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=53 lang=typescript
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+export function maxSubArray(nums: number[]): number {
+  let sofar = nums[0], end = 0;
+
+  for(const cur of nums ){
+    if( end < 0 ) end = cur;
+    else end += cur;
+
+    if( end > sofar ) sofar = end;
+  }
+
+  return sofar;
+}
+// @lc code=end
+
