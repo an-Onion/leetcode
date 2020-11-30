814ac32ed9a9a906d806085b49724845581c6b54
ma_c <ma_c@worksap.co.jp>
2020-11-30 20:00:08 +0800

    dp of ebay

---
+++ src/416.partition-equal-subset-sum.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=416 lang=typescript
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+export function canPartition(nums: number[]): boolean {
+  const dp = [[1]];
+  let MaX = 0;
+
+  for(let i = 1; i <= nums.length; ++i ){
+    const cur = nums[i-1];
+    dp.push([]);
+    for(let diff = 0; diff <= MaX; ++diff){
+      if( i === 0 || dp[i-1][diff] ){
+        dp[i][Math.abs(diff+cur)] = 1;
+        dp[i][Math.abs(diff-cur)] = 1;
+      }
+    }
+
+    MaX += cur;
+  }
+  return dp[nums.length][0] === 1;
+}
+// @lc code=end
+
