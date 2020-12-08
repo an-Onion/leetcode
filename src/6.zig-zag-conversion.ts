077dbf9d35de877ff0a502ea923dad6aaa9b868f
ma_c <ma_c@worksap.co.jp>
2020-12-08 16:59:50 +0800

    6 zig zag conversion

---
+++ src/6.zig-zag-conversion.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=6 lang=typescript
+ *
+ * [6] ZigZag Conversion
+ */
+
+// @lc code=start
+export function convert(s: string, numRows: number): string {
+
+  if( numRows === 1) return s;
+
+  const T = (numRows - 1) * 2;
+
+  const idx = (i) => i % T < numRows ? (i % T) : (T - i%T);
+
+  const row : string[] = Array(numRows).fill('');
+
+  for(let i = 0; i < s.length; i++){
+    const j = idx(i);
+    row[j] += s[i];
+  }
+
+  return row.join('');
+}
+// @lc code=end
+
---
+++ test/6.zig-zag-conversion.test.ts
@@ -0,0 +1,15 @@
+import { convert } from '../src/6.zig-zag-conversion';
+
+describe('ZigZag Conversion', () => {
+  it('PAYPALISHIRING 3', () => {
+    expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
+  });
+
+  it('PAYPALISHIRING 4', () => {
+    expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
+  });
+
+  it('A 1', () => {
+    expect(convert('A', 1)).toBe('A');
+  });
+});
