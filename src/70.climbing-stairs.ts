8c78693088b75b6b259b506badb6a53f124ae708
ma_c <ma_c@worksap.co.jp>
2020-12-11 20:16:34 +0800

    dp basically

---
+++ src/70.climbing-stairs.ts
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=70 lang=typescript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+export function climbStairs(n: number): number {
+  const dp = [1, 1];
+
+  for(let i = 2; i <=n; ++i )
+    dp[ i & 1 ] = dp[ i-1 & 1 ] + dp[ i-2 & 1];
+
+  return dp[n & 1];
+}
+// @lc code=end
+
---
+++ test/70.climbing-stairs.test.ts
@@ -0,0 +1,13 @@
+import { climbStairs } from '../src/70.climbing-stairs';
+
+describe('Climbing Stairs', () => {
+
+  it('2', () => {
+    expect(climbStairs(2)).toBe(2);
+  });
+
+  it('3', () => {
+    expect(climbStairs(3)).toBe(3);
+  });
+
+});
