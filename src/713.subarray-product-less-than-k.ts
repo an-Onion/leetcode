e282f024d4741939eb0fe3f283dce8ab9632e533
ma_c <ma_c@worksap.co.jp>
2020-12-14 20:01:55 +0800

    sliding window

---
+++ src/713.subarray-product-less-than-k.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=713 lang=typescript
+ *
+ * [713] Subarray Product Less Than K
+ */
+
+// @lc code=start
+export function numSubarrayProductLessThanK(nums: number[], k: number): number {
+
+  if( k <= 1 ) return 0;
+
+  let [res, prod] = [0, 1];
+
+  for( let left =0, right=0; right < nums.length; ++right){
+    prod *= nums[right];
+
+    while(prod >= k) prod /= nums[left++];
+
+    res += right - left + 1;
+  }
+
+  return res;
+}
+// @lc code=end
+
---
+++ test/713.subarray-product-less-than-k.test.ts
@@ -0,0 +1,12 @@
+import { numSubarrayProductLessThanK } from '../src/713.subarray-product-less-than-k';
+
+describe('Subarray Product Less Than K', () => {
+  it('nums = [10, 5, 2, 6], k = 100', () => {
+    expect(numSubarrayProductLessThanK([10, 5, 2, 6], 100)).toBe(8);
+  });
+
+  it('nums = [10, 5, 2, 6], k = 601', () => {
+    expect(numSubarrayProductLessThanK([10, 5, 2, 6], 601)).toBe(10);
+  });
+
+});
