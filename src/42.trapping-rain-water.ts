7ee0cdc11a9c512db58006601fff3a3e71ff15b6
ma_c <ma_c@worksap.co.jp>
2020-12-23 15:33:45 +0800

    2 pointer with water

---
+++ src/42.trapping-rain-water.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=42 lang=typescript
+ *
+ * [42] Trapping Rain Water
+ */
+
+// @lc code=start
+export function trap(height: number[]): number {
+
+  let l = 0, r = height.length -1;
+  let [L, R, water] = [0,0,0];
+  while( l < r) {
+    L = Math.max(L, height[l]);
+    R = Math.max(R, height[r]);
+    if( L < R )
+      water += L - height[l++];
+    else
+      water += R - height[r--];
+  }
+
+  return water;
+}
+// @lc code=end
+
---
+++ test/42.trapping-rain-water.test.ts
@@ -0,0 +1,28 @@
+import { trap } from '../src/42.trapping-rain-water';
+
+describe('Trapping Rain Water', () => {
+
+  it('[1]', () => {
+    expect(trap([1])).toBe(0);
+  });
+
+  it('[0, 1]', () => {
+    expect(trap([0, 1])).toBe(0);
+  });
+
+  it('[0, 1, 0 , 2]', () => {
+    expect(trap([0, 1, 0 , 2])).toBe(1);
+  });
+
+  it('[ 2, 1, 0, 1, 3]', () => {
+    expect(trap([2, 1, 0, 1, 3])).toBe(4);
+  });
+
+  it('[0,1,0,2,1,0,1,3,2,1,2,1]', () => {
+    expect(trap([0,1,0,2,1,0,1,3,2,1,2,1])).toBe(6);
+  });
+
+  it('[4,2,0,3,2,5]', () => {
+    expect(trap([4,2,0,3,2,5])).toBe(9);
+  });
+});
