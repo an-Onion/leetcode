2c97a47c3b91df986311a04debccba96a6e31679
ma_c <ma_c@worksap.co.jp>
2021-01-07 12:51:38 +0800

    very hard one

---
+++ src/22.generate-parentheses.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=22 lang=typescript
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+export function generateParenthesis(n: number): string[] {
+
+  const res: string[] = [];
+
+  function DFS(left: number, right: number, sub:string){
+    if( !right ) res.push(sub);
+    if( left > 0 ) DFS(left-1, right, sub+'(');
+    if(right > left ) DFS(left, right-1, sub+')');
+  }
+  DFS(n, n, '');
+  return res;
+}
+// @lc code=end
+
