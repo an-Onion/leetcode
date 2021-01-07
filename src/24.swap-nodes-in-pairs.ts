6e42a56acba8d1099598b7172d9553b10e511168
ma_c <ma_c@worksap.co.jp>
2021-01-07 16:56:15 +0800

    complicated link list

---
+++ src/24.swap-nodes-in-pairs.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=24 lang=typescript
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+import { ListNode } from '../src/dataStructure/ListNode';
+
+export function swapPairs(head: ListNode | null): ListNode | null {
+
+  const dummy: ListNode = new ListNode(-1, head);
+
+  let q = head, pre = dummy;
+
+  while( q && q.next ){
+    pre.next = q.next;
+    pre = q;
+    q = q.next;
+    pre.next = q.next;
+    q.next = pre;
+    q = pre.next;
+  }
+
+  return dummy.next;
+}
+// @lc code=end
+
