ace2b3a322f996fade7ca92235b8f8ed29d4edda
ma_c <ma_c@worksap.co.jp>
2021-01-18 18:37:17 +0800

    first missing positive, a hard problem

---
+++ src/41.first-missing-positive.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=41 lang=typescript
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+export function firstMissingPositive(nums: number[]): number {
+
+  for(let i = 0; i < nums.length; i++) {
+    while ( 0 < nums[i] && nums[i] <= i ){
+      const j = nums[i] - 1;
+      if( nums[i] === nums[j] )
+        nums[i] = -1;
+      else
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  }
+
+  let res = 1;
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] === i+1) res++;
+    else return res;
+  }
+
+  return res;
+}
+// @lc code=end
+
---
+++ test/41.first-missing-positive.test.ts
@@ -0,0 +1,20 @@
+import { firstMissingPositive } from '../src/41.first-missing-positive';
+
+describe('First Missing Positive', () => {
+
+  it('[2,1]', () => {
+    expect(firstMissingPositive([2,1])).toBe(3);
+  });
+
+  it('[1,2,0]', () => {
+    expect(firstMissingPositive([1,2,0])).toBe(3);
+  });
+
+  it('[3,4,-1,1]', () => {
+    expect(firstMissingPositive([3,4,-1,1])).toBe(2);
+  });
+
+  it('[7,8,9,11,12]', () => {
+    expect(firstMissingPositive([7,8,9,11,12])).toBe(1);
+  });
+});
