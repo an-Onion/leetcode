9c162a860e985a9228db0ee80885dfb78eb26a9d
ma_c <ma_c@worksap.co.jp>
2021-01-20 16:34:41 +0800

    manual pow

---
+++ src/50.pow-x-n.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=50 lang=typescript
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+export function myPow(x: number, n: number): number {
+
+  let ans = 1;
+
+  if(n === -2147483648) {
+    ans = x, n = -2147483647;
+  }
+
+  let exp = Math.abs(n);
+
+  while( exp > 0 ){
+    if( exp & 1 )  ans *= x;
+    exp >>= 1;
+    x *= x;
+  }
+
+  return n < 0 ? 1/ans : ans;
+}
+// @lc code=end
+
---
+++ test/50.pow-x-n.test.ts
@@ -0,0 +1,15 @@
+import { myPow } from '../src/50.pow-x-n';
+
+describe('Pow(x, n)', () => {
+  it('2, 10', () => {
+    expect(myPow(2, 10)).toBe(1024);
+  });
+
+  it('2.1, 3', () => {
+    expect(myPow(2.1, 3).toFixed(3)).toBe('9.261');
+  });
+
+  it('2, -2', () => {
+    expect(myPow(2, -2)).toBe(0.25);
+  });
+});
