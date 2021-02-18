b368ad0c174b53437237c5781642e6e23c8b652f
ma_c <ma_c@worksap.co.jp>
2021-02-18 20:17:51 +0800

    7 years ago, I submit this problem

---
+++ src/80.remove-duplicates-from-sorted-array-ii.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=80 lang=typescript
+ *
+ * [80] Remove Duplicates from Sorted Array II
+ */
+
+// @lc code=start
+export function removeDuplicates(nums: number[]): number {
+   let res = 0, limit = 0;
+
+   for(let i = 0; i < nums.length; i++) {
+     if( nums[i] !== nums[i-1]) limit = 0;
+
+     if( ++limit > 2 ) continue;
+
+     nums[res++] = nums[i];
+   }
+
+   nums.length = res;
+
+   return res;
+}
+// @lc code=end
+
