f18f93f89726e47d41751a272220e1f80c31bc39
ma_c <ma_c@worksap.co.jp>
2021-03-01 20:33:05 +0800

    another Monday

---
+++ src/121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=121 lang=typescript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+export function maxProfit(prices: number[]): number {
+
+  let [sofar, min] = [0, Infinity];
+
+  for(const p of prices) {
+    if( p - min > sofar ) sofar = p - min;
+    if( p < min ) min = p;
+  }
+  return sofar;
+}
+// @lc code=end
+
---
+++ test/121.best-time-to-buy-and-sell-stock.test.ts
@@ -0,0 +1,13 @@
+import { maxProfit } from '../src/121.best-time-to-buy-and-sell-stock';
+
+describe('Best Time to Buy and Sell Stock', () => {
+
+  it('[7,1,5,3,6,4]', () => {
+    expect(maxProfit([7,1,5,3,6,4])).toBe(5);
+  });
+
+  it('[7,6,4,3,1]', () => {
+    expect(maxProfit([7,6,4,3,1])).toBe(0);
+  });
+
+});
