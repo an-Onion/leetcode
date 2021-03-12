e95eccad1fd94caaea3a3a22ee2f0d398905d987
ma_c <ma_c@worksap.co.jp>
2021-03-12 16:12:45 +0800

    only one

---
+++ src/199.binary-tree-right-side-view.ts
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=199 lang=typescript
+ *
+ * [199] Binary Tree Right Side View
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+import { TreeNode } from './dataStructure/TreeNode';
+export function rightSideView(root: TreeNode | null): number[] {
+
+  if( !root ) return [];
+
+  const queue = [root];
+  let count = 1;
+  const ret = [];
+  while(queue.length) {
+
+    const node = queue.shift();
+
+    node.left && queue.push(node.left);
+    node.right && queue.push(node.right);
+
+    if(--count) continue;
+
+    count = queue.length;
+    ret.push(node.val);
+  }
+  return ret;
+}
+// @lc code=end
+
---
+++ test/199.binary-tree-right-side-view.test.ts
@@ -0,0 +1,21 @@
+import { TreeNode } from '../src/dataStructure/TreeNode';
+import { rightSideView } from '../src/199.binary-tree-right-side-view';
+
+describe('Binary Tree Right Side View', () => {
+
+  it('[1,2,3,null,5,null,4]', () => {
+    const input = TreeNode.of([1,2,3,null,5,null,4]);
+    expect(rightSideView(input)).toEqual([1,3,4]);
+  });
+
+  it('[1,null,3]', () => {
+    const input = TreeNode.of([1,null,3]);
+    expect(rightSideView(input)).toEqual([1,3]);
+  });
+
+  it('[]', () => {
+    const input = TreeNode.of([]);
+    expect(rightSideView(input)).toEqual([]);
+  });
+
+});
