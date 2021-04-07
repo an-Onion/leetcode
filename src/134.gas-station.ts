c6e2f7343128ffab7c8ae5a497f5524795822f0e
ma_c <ma_c@worksap.co.jp>
2021-04-07 11:50:21 +0800

    long time no coding

---
+++ src/134.gas-station.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=134 lang=typescript
+ *
+ * [134] Gas Station
+ */
+
+// @lc code=start
+export function canCompleteCircuit(gas: number[], cost: number[]): number {
+
+  const N = gas.length;
+
+  let start = N, end = 0, tank = 0, cur = 0;
+
+  for(let i = 0; i < N; i++) {
+
+    tank += gas[cur] - cost[cur];
+
+    if( tank >= 0 )
+      cur = ++end;
+    else
+      cur = --start;
+  }
+
+
+  return tank >= 0 ? (start % N) : -1;
+}
+// @lc code=end
+
---
+++ test/134.gas-station.test.ts
@@ -0,0 +1,35 @@
+import { canCompleteCircuit } from '../src/134.gas-station';
+
+describe('Gas Station', () => {
+
+  it('gas = [1,2,3,4,5], cost = [3,4,5,1,2]', () => {
+    expect(canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2])).toBe(3);
+  });
+
+  it('gas = [2,3,4], cost = [3,4,3]', () => {
+    expect(canCompleteCircuit([2,3,4], [3,4,3])).toBe(-1);
+  });
+
+  it('gas = [2], cost = [3]', () => {
+    expect(canCompleteCircuit([2], [3])).toBe(-1);
+  });
+
+  it('gas = [2], cost = [2]', () => {
+    expect(canCompleteCircuit([2], [2])).toBe(0);
+  });
+
+
+  it('gas = [5,1,2,3,4], cost = [4,4,1,5,1]', () => {
+    expect(canCompleteCircuit([5,1,2,3,4], [4,4,1,5,1])).toBe(4);
+  });
+
+  it('gas = [4,5,2,6,5,3], cost = [3,2,7,3,2,9]', () => {
+    expect(canCompleteCircuit([4,5,2,6,5,3], [3,2,7,3,2,9])).toBe(-1);
+  });
+
+
+  it('gas = [3,1,1], cost = [1,2,2]', () => {
+    expect(canCompleteCircuit([3,1,1], [1,2,2])).toBe(0);
+  });
+
+});
