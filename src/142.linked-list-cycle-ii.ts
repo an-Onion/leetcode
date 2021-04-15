c5584c6e4cbf7de096f09b221eb596e44ac7e1bc
ma_c <ma_c@worksap.co.jp>
2021-04-15 17:36:56 +0800

    only one problem I can solve today

---
+++ src/142.linked-list-cycle-ii.ts
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=142 lang=typescript
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import { ListNode } from './dataStructure/ListNode';
+
+export function detectCycle(head: ListNode | null): ListNode | null {
+
+  let p = hasCycle();
+  if( !p ) return null;
+
+  let q = head;
+
+  while(p !== q){
+    p = p.next;
+    q = q.next;
+  }
+
+  return p;
+
+  function hasCycle(): ListNode | null{
+
+    let fast = head, slow = head;
+
+    while(slow && fast){
+      slow = slow.next;
+      fast = fast?.next?.next;
+      if( slow === fast ) return slow;
+    }
+
+    return null;
+  }
+}
+// @lc code=end
+
---
+++ test/142.linked-list-cycle-ii.test.ts
@@ -0,0 +1,30 @@
+import { detectCycle } from '../src/142.linked-list-cycle-ii';
+import { ListNode } from '../src/dataStructure/ListNode';
+
+describe('Linked List Cycle II', () => {
+
+  it('head = [3,2,0,-4], pos = 1', () => {
+    const head = ListNode.of([3,2,0,-4]);
+    const tail = ListNode.getTail(head);
+    tail.next = head.next;
+    const output = detectCycle(head);
+    expect(output.val).toBe(2);
+  });
+
+  it('head = [1,2], pos = 0', () => {
+    const head = ListNode.of([1,2]);
+    const tail = ListNode.getTail(head);
+    tail.next = head;
+    const output = detectCycle(head);
+    expect(output.val).toBe(1);
+  });
+
+  it('head = [1], pos = -1', () => {
+    const head = ListNode.of([1]);
+    const output = detectCycle(head);
+    expect(output).toBeNull();
+  });
+
+
+});
+
