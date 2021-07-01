2bb295197cdaff6e8acdfa2a3533277c0e5be3bb
ma_c <ma_c@worksap.co.jp>
2021-07-01 18:29:35 +0800

    Priority Queue for typescript

---
+++ src/1046.last-stone-weight.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1046 lang=typescript
+ *
+ * [1046] Last Stone Weight
+ */
+
+// @lc code=start
+
+import { PriorityQueue } from 'typescript-collections';
+
+export function lastStoneWeight(stones: number[]): number {
+  const Q = new PriorityQueue<number>();
+
+  stones.forEach((stone) => Q.enqueue(stone));
+
+  while( Q.size() > 1 ){
+    const first = Q.dequeue();
+    const second = Q.dequeue();
+    if( first === second ) continue;
+
+    Q.enqueue(first -second);
+  }
+
+  const res = Q.dequeue();
+
+  return res || 0;
+}
+// @lc code=end
+
