04ec5e0c432b7d9ac4e3bcba45ddead92930b545
ma_c <ma_c@worksap.co.jp>
2021-07-12 20:15:48 +0800

    Map training

---
+++ src/1436.destination-city.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1436 lang=typescript
+ *
+ * [1436] Destination City
+ */
+
+// @lc code=start
+export function destCity(paths: string[][]): string {
+
+  const roads = new Map();
+
+  for (const path of paths) {
+      roads.set(path[0], path[1]);
+  }
+
+  let dest = paths[0][0];
+
+  while (roads.has(dest)) {
+      dest = roads.get(dest);
+  }
+  return dest;
+}
+// @lc code=end
+
---
+++ test/1436.destination-city.test.ts
@@ -0,0 +1,23 @@
+import { destCity } from '../src/1436.destination-city';
+
+describe('Destination City', () => {
+
+  it('Sao Paulo', () => {
+    const paths = [['London','New York'],['New York','Lima'],['Lima','Sao Paulo']];
+
+    expect(destCity(paths)).toBe('Sao Paulo');
+  });
+
+  it('A', () => {
+    const paths = [['B','C'],['D','B'],['C','A']];
+
+    expect(destCity(paths)).toBe('A');
+  });
+
+  it('Z', () => {
+    const paths = [['A','Z']];
+
+    expect(destCity(paths)).toBe('Z');
+  });
+
+});
