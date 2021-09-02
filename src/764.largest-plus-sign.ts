5f422be1cfc2e62c75098ea8d8a855080c59c941
ma_c <ma_c@worksap.co.jp>
2021-09-02 21:11:40 +0800

    only one can do today

---
+++ src/764.largest-plus-sign.ts
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=764 lang=typescript
+ *
+ * [764] Largest Plus Sign
+ */
+
+// @lc code=start
+export function orderOfLargestPlusSign(n: number, mines: number[][]): number {
+
+  const matrix = Array(n).fill(0).map(() => Array(n).fill(n));
+
+  for(const mine of mines) {
+    matrix[mine[0]][mine[1]] = 0;
+  }
+
+  for(let r = 0; r < n; ++r){
+    let count = 0;
+
+    for(let c = 0; c < n; ++c){
+      if(!matrix[r][c]){
+        count = 0;
+        continue;
+      }
+      matrix[r][c] = Math.min(++count, matrix[r][c]);
+    }
+
+    count = 0;
+    for(let c = n-1; c >= 0; --c){
+      if(!matrix[r][c]){
+        count = 0;
+        continue;
+      }
+      matrix[r][c] = Math.min(++count, matrix[r][c]);
+    }
+  }
+
+  let ans = 0;
+  for(let c = 0; c < n; ++c){
+    let count = 0;
+    for(let r = 0; r < n; ++ r){
+      if(!matrix[r][c]){
+        count = 0;
+        continue;
+      }
+      matrix[r][c] = Math.min(++count, matrix[r][c]);
+    }
+
+    count = 0;
+    for(let r = n-1; r >= 0; --r){
+      if(!matrix[r][c]){
+        count = 0;
+        continue;
+      }
+      matrix[r][c] = Math.min(++count, matrix[r][c]);
+      ans = Math.max(matrix[r][c], ans);
+    }
+  }
+
+  return ans;
+}
+// @lc code=end
+
---
+++ test/764.largest-plus-sign.test.ts
@@ -0,0 +1,15 @@
+import { orderOfLargestPlusSign } from '../src/764.largest-plus-sign';
+
+describe('Largest Plus Sign', () => {
+
+  it('e.g. 1', () => {
+    const n = 5, mines = [[4,2]];
+    expect(orderOfLargestPlusSign(n, mines)).toBe(2);
+  });
+
+  it('e.g. 2', () => {
+    const n = 1, mines = [[0,0]];
+    expect(orderOfLargestPlusSign(n, mines)).toBe(0);
+  });
+
+});
