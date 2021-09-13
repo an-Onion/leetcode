dc750af9c3958862a7747f91f06c868900f3e753
ma_c <ma_c@worksap.co.jp>
2021-09-13 20:25:50 +0800

    easy one today

---
+++ src/872.leaf-similar-trees.ts
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=872 lang=typescript
+ *
+ * [872] Leaf-Similar Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+import { TreeNode } from './dataStructure/TreeNode';
+
+export function leafSimilar( root1: TreeNode | null, root2: TreeNode | null ): boolean {
+
+  const leaves1 = DFS( root1, ( ret ) => ret );
+  const leaves2 = DFS( root2, ( ret ) => ret );
+  return leaves1.toString() === leaves2.toString();
+
+  function DFS( node: TreeNode | null, next: ( leaves: number[] ) => number[] ): number[] {
+    if( !node ) return next( [] );
+    if( !node.left && !node.right ) return next( [node.val] );
+
+    return DFS( node.left, ( left ) => DFS( node.right, ( right ) => next( [...left, ...right] ) ) );
+  }
+
+}
+// @lc code=end
+
---
+++ test/872.leaf-similar-trees.test.ts
@@ -0,0 +1,25 @@
+import { leafSimilar } from '../src/872.leaf-similar-trees';
+import { TreeNode } from '../src/dataStructure/TreeNode';
+
+describe( 'Leaf-Similar Trees', () => {
+
+  it( 'e.g. 1', () => {
+    const root1 = TreeNode.of( [3,5,1,6,2,9,8,null,null,7,4] );
+    const root2 = TreeNode.of( [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8] );
+    expect( leafSimilar( root1, root2 ) ).toBeTruthy();
+  } );
+
+  it( 'e.g. 2', () => {
+    const root1 = TreeNode.of( [1] );
+    const root2 = TreeNode.of( [1] );
+    expect( leafSimilar( root1, root2 ) ).toBeTruthy();
+  } );
+
+  it( 'e.g. 3', () => {
+    const root1 = TreeNode.of( [1] );
+    const root2 = TreeNode.of( [2] );
+    expect( leafSimilar( root1, root2 ) ).toBeFalsy();
+  } );
+
+
+} );
