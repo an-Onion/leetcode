920d318b611854d75f642fc89b99069164ed6d46
Onion <congma@outlook.com>
2024-03-07 22:16:14 +0800

    an easy problem for it's tired today

--- src/67.add-binary.ts
+++ src/67.add-binary.ts
@@ -7,21 +7,18 @@
 // @lc code=start
 export function addBinary( a: string, b: string ): string {
 
-  const binary = ( c: string | undefined ): number => parseInt( c || '0' );
+  const ret = [];
+  const A = a.split( '' ).reverse().map( ( v ) => parseInt( v, 10 ) );
+  const B = b.split( '' ).reverse().map( ( v ) => parseInt( v, 10 ) );
 
-  const arrA = a.split( '' ).reverse(), arrB = b.split( '' ).reverse();
-  const res: number[] = [];
   let carry = 0;
-
-  for( let i = 0; i < arrA.length || i < arrB.length; ++i ){
-    const sum = binary( arrA[i] ) + binary( arrB[i] ) + carry;
-    res.push( sum & 1 );
+  for( let i = 0; i < A.length || i < B.length; i++ ){
+    const sum = ( A[i] || 0 ) + ( B[i] || 0 ) + carry;
+    ret.push( sum % 2 );
     carry = sum >> 1;
   }
-
-  if( carry ) res.push ( 1 );
-
-  return res.reverse().join( '' );
+  if( carry ) ret.push( carry );
+  return ret.reverse().join( '' );
 }
 // @lc code=end
 
