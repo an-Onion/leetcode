ff9a858807e9c24b5539ec04f75601b64d5d2815
Onion <congma@outlook.com>
2024-03-31 15:13:41 +0800

    Actaully, I can't fully understand why candy is a hard problem

---
+++ src/133.clone-graph.test.ts
@@ -0,0 +1,43 @@
+export class Node {
+    val: number;
+    neighbors: Node[];
+    constructor( val?: number, neighbors?: Node[] ) {
+        this.val = ( val === undefined ? 0 : val );
+        this.neighbors = ( neighbors === undefined ? [] : neighbors );
+    }
+}
+
+function cloneGraph( node: Node | null ): Node | null {
+
+    const visited = new Map<Node, Node>();
+
+    function DFS( node: Node ): Node {
+        
+        if( !node ) return null;
+        if( visited.has( node ) ) return visited.get( node )!;
+
+        const clone = new Node( node.val );
+        visited.set( node, clone );
+
+        for( const neighbor of node.neighbors ) {
+            const newNeighbor = DFS( neighbor );
+            clone.neighbors.push( newNeighbor );
+        }
+        return clone;
+    }
+    return DFS( node );
+}
+
+describe( '133. clone graph', () => {
+    
+    it( 'should work', () => {
+        const node = new Node( 1 );
+        node.neighbors = [ new Node( 2 ), new Node( 3 ), new Node( 4 ) ];
+        node.neighbors[ 0 ].neighbors = [ new Node( 5 ) ];
+        node.neighbors[ 1 ].neighbors = [ new Node( 6 ) ];
+
+        const clone = cloneGraph( node );
+        expect( clone ).toEqual( node );
+    } );
+
+} );
---
+++ src/135.candy.test.ts
@@ -0,0 +1,47 @@
+function candy( ratings: number[] ): number {
+
+    if ( ratings.length === 0 ) return 0;
+
+    let [pre, ret, inc, dec] = [1, 1, 1, 1];
+
+    for ( let i = 1; i < ratings.length; i++ ) {
+
+        if ( ratings[i] === ratings[i - 1] ) {
+            [pre, ret, inc, dec ] = [1, ret + 1, 1, 1];
+            continue;
+        }
+
+        if ( ratings[i] > ratings[i - 1] ) {
+            dec = 1;
+            ret += ++pre;
+            inc = pre;
+            continue;
+        }
+        if ( dec == inc ) {
+            dec++;
+        }
+        ret += dec++;
+        pre = 1;
+    }
+    return ret;
+}
+
+describe( '135. candy', () => {
+
+    it( '1,0,2', () => {
+        expect( candy( [1, 0, 2] ) ).toEqual( 5 );
+    } );
+
+    it( '1,2,2', () => {
+        expect( candy( [1, 2, 2] ) ).toEqual( 4 );
+    } );
+
+    it( '1,2,3', () => {
+        expect( candy( [1, 2, 3] ) ).toEqual( 6 );
+    } );
+
+    it( '1,3,2,2,1', () => {
+        expect( candy( [1, 3, 2, 2, 1] ) ).toEqual( 7 );
+    } );
+
+} );
