7d2d35102cfa6d7048739cdced72312229d9b81a
Onion <congma@outlook.com>
2024-03-31 13:04:28 +0800

    2 Hard in the morning, but the sconde one is a little confusion aobut 3 nested loop.

---
+++ src/124.binary-tree-maximum-path-sum.test.ts
@@ -0,0 +1,40 @@
+import { TreeNode } from './dataStructure/TreeNode';
+
+function maxPathSum( root: TreeNode | null ): number {
+
+    const sub = new Map<TreeNode | null, number>();
+
+    function DFS( node: TreeNode | null , next: ( ret: number ) => void ): void {
+        
+        if( !node ) return next( 0 );
+
+        return DFS( node.left, ( left ) => {
+            return DFS( node.right, ( right ) => {
+                const ret = Math.max( left, 0 ) + Math.max( right, 0 ) + node.val;
+                sub.set( node, ret );
+                return next( Math.max( 0, left, right  ) + + node.val );
+            } );
+        } );
+    }
+
+    DFS( root, () => {} );
+
+    return Math.max( ...sub.values() );
+}
+
+describe( '124. binary-tree-maximum-path-sum', () => {
+    it( '1,2,3', () => {
+        const root = TreeNode.of( [1,2,3] );
+        expect( maxPathSum( root ) ).toBe( 6 );
+    } );
+
+    it( '-10,9,20,null,null,15,7', () => {
+        const root = TreeNode.of( [-10,9,20,null,null,15,7] );
+        expect( maxPathSum( root ) ).toBe( 42 );
+    } );
+
+    it( '5,4,8,11,null,13,4,7,2,null,null,null,1', () => {
+        const root = TreeNode.of( [5,4,8,11,null,13,4,7,2,null,null,null,1] );
+        expect( maxPathSum( root ) ).toBe( 48 );
+    } );
+} );
---
+++ src/132.palindrome-partitioning-ii.test.ts
@@ -0,0 +1,52 @@
+import * as testCase from './largeData/132.json';
+
+function minCut( s: string ) {
+    const N = s.length;
+    const dp = Array.from( { length: N }, () => new Array( N ).fill( true ) );
+
+    for ( let i = N - 1; i >= 0; --i ) {
+        for ( let j = i + 1; j < N; ++j ) {
+            dp[i][j] = s[i] == s[j] && dp[i + 1][j - 1];
+        }
+    }
+
+    const ret = new Array( N ).fill( Infinity );
+
+    for ( let i = 0; i < N; ++i ) {
+        if ( dp[0][i] ) {
+            ret[i] = 0;
+            continue;
+        }
+        for ( let j = 0; j < i; ++j ) {
+            if ( !dp[j + 1][i] ) continue;
+            ret[i] = Math.min( ret[i], ret[j] + 1 );
+        }
+    }
+
+    return ret[N - 1];
+}
+
+
+describe( '132. palindrome-partitioning-ii', () => {
+
+    it( 'aab', () => {
+        expect( minCut( 'aab' ) ).toBe( 1 );
+    } );
+
+    it( 'a', () => {
+        expect( minCut( 'a' ) ).toBe( 0 );
+    } );
+
+    it( 'ab', () => {
+        expect( minCut( 'ab' ) ).toBe( 1 );
+    } );
+
+    it( 'aabaccdb', () => {
+        expect( minCut( 'aabaccdb' ) ).toBe( 4 );
+    } );
+
+    it( 'large 1', () => {
+        const input = testCase[0];
+        expect( minCut( input ) ).toBe( 1345 );
+    } );
+} );
