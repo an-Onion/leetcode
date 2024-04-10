15d26534245f1ae2cdf80814503a438a84fad86e
Onion <congma@outlook.com>
2024-04-10 21:53:19 +0800

    another 2 medium day

---
+++ src/172.factorial-trailing-zeroes.test.ts
@@ -0,0 +1,29 @@
+function trailingZeroes( n: number ): number {
+  
+    let five = 5, ret = 0;
+
+    while( n / five >= 1 ){
+        ret += Math.floor( n / five );
+        five *= 5;
+    }
+    return ret;
+}
+
+describe( '172. factorial-trailing-zeroes', () => {
+
+    it( '25', () => {
+        expect( trailingZeroes( 25 ) ).toBe( 6 );
+    } );
+
+    it( '3', () => {
+        expect( trailingZeroes( 3 ) ).toBe( 0 );
+    } );
+
+    it( '5', () => {
+        expect( trailingZeroes( 5 ) ).toBe( 1 );
+    } );
+
+    it( '0', () => {
+        expect( trailingZeroes( 0 ) ).toBe( 0 );
+    } );
+} );
---
+++ src/173.binary-search-tree-iterator.test.ts
@@ -0,0 +1,51 @@
+import { TreeNode } from './dataStructure/TreeNode';
+
+class BSTIterator {
+
+    stack: TreeNode[];
+    current: TreeNode | null;
+
+    constructor( root: TreeNode | null ) {
+        this.stack = [];
+        this.current = this.init( root );
+    }
+
+    init( node: TreeNode ) {
+        while( node.left ) {
+            this.stack.push( node );
+            node = node.left;
+        }
+        return node;
+    }
+
+    next(): number {
+        const node = this.current;
+        if( node.right ){
+            this.current = this.init( node.right );
+            return node.val;
+        } 
+        this.current = this.stack.pop();
+
+        return node.val;
+    }
+
+    hasNext(): boolean {
+        return !!this.current;
+    }
+}
+
+describe( '173. binary-search-tree-iterator', () => {
+    it( '[7,3,15,null,null,9,20]', () => {
+        const root = TreeNode.of( [7,3,15,null,null,9,20] );
+        const iterator = new BSTIterator( root );
+        expect( iterator.next() ).toBe( 3 );
+        expect( iterator.next() ).toBe( 7 );
+        expect( iterator.hasNext() ).toBe( true );
+        expect( iterator.next() ).toBe( 9 );
+        expect( iterator.hasNext() ).toBe( true );
+        expect( iterator.next() ).toBe( 15 );
+        expect( iterator.hasNext() ).toBe( true );
+        expect( iterator.next() ).toBe( 20 );
+        expect( iterator.hasNext() ).toBe( false );
+    } );
+} );
