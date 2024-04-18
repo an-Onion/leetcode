a9f95045218091a4126fd7d257dc89dedd180cc3
Onion <congma@outlook.com>
2024-04-18 23:47:37 +0800

    fast solve a water problem

---
+++ src/201-300/226.invert-binary-tree.test.ts
@@ -0,0 +1,32 @@
+import { TreeNode } from '../dataStructure/TreeNode';
+
+function invertTree( root: TreeNode | null ): TreeNode | null {
+    if( !root ) return null;
+
+    const left = invertTree( root.left );
+    const right = invertTree( root.right );
+    root.left = right;
+    root.right = left;
+    return root;
+}
+
+describe( '226. invert-binary-tree', () => {
+    it( '[4,2,7,1,3,6,9]', () => {
+        const root = TreeNode.of( [4,2,7,1,3,6,9] );
+        const output = invertTree( root );
+        expect( TreeNode.toArray( output ) ).toStrictEqual( [4,7,2,9,6,3,1] );
+    } );
+
+    it( '[2,1,3]', () => {
+        const root = TreeNode.of( [2,1,3] );
+        const output = invertTree( root );
+        expect( TreeNode.toArray( output ) ).toStrictEqual( [2,3,1] );
+    } );
+
+    it( '[]', () => {
+        const root = TreeNode.of( [] );
+        const output = invertTree( root );
+        expect( TreeNode.toArray( output ) ).toStrictEqual( [] );
+        
+    } );
+} );
