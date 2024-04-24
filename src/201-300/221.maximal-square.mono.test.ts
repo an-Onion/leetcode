3e04cf58bf6188b33ae17681c811767eaccf408a
Onion <congma@outlook.com>
2024-04-24 21:56:57 +0800

    long time no mono stack

---
+++ src/201-300/221.maximal-square.mono.test.ts
@@ -0,0 +1,51 @@
+function maximalSquare( matrix: string[][] ): number {
+    const M = matrix.length, N = matrix[0].length;
+    const h = new Array( N ).fill( 0 );
+    const mono: number[] = [-1];
+
+    let ret = 0;
+
+    for( let i = 0; i < M; i++ ) {
+        for( let j = 0; j < N; j++ ){
+            h[j] = matrix[i][j] === '1' ? h[j] + 1 : 0;
+
+            while( h[j] < h[mono.at( -1 )] ){
+                const top = mono.pop();
+                const side = Math.min( h[top], j - mono.at( -1 ) - 1 );
+                ret = Math.max( ret, side * side );
+            }
+
+            mono.push( j );
+        }
+        while( mono.length > 1 ){
+            const top = mono.pop();
+            const side = Math.min( h[top], N - mono.at( -1 ) - 1 );
+            ret = Math.max( ret, side * side );
+        }
+    }
+
+    return ret;
+}
+
+describe( '221. maximal-square', () => {
+    
+    it( 'case 1', () => {
+        const matrix = [
+            ['1','0','1','0','0'],
+            ['1','0','1','1','1'],
+            ['1','1','1','1','1'],
+            ['1','0','0','1','0'],
+        ];
+        expect( maximalSquare( matrix ) ).toBe( 4 );
+    } );
+
+    it( 'case 2', () => {
+        const matrix = [['0','1'],['1','0']];
+        expect( maximalSquare( matrix ) ).toBe( 1 );
+    } );
+
+    it( 'case 3', () => {
+        const matrix = [['0']];
+        expect( maximalSquare( matrix ) ).toBe( 0 );
+    } );
+} );
--- src/201-300/223.rectangle-area.ts
+++ src/201-300/223.rectangle-area.ts
@@ -9,14 +9,13 @@ export function computeArea( ax1: number, ay1: number, ax2: number, ay2: number,
 
   const area = ( ax2 - ax1 ) * ( ay2 - ay1 ) + ( bx2 - bx1 ) * ( by2 - by1 );
 
-  if( bx1 > ax2 || by2  < ay1 || bx2 < ax1 || by1 > ay2 ){
+  if( ax1 > bx2 || ax2 < bx1 || ay1 > by2 || ay2 < by1 )
     return area;
-  }
 
   const x = Math.min( ax2, bx2 ) - Math.max( ax1, bx1 );
   const y = Math.min( ay2, by2 ) - Math.max( ay1, by1 );
-  const  overlap = x * y;
-  return  area - overlap;
+
+  return area - x * y;
 }
 // @lc code=end
 
