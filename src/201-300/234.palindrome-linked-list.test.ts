2338fa031ba259c5c8b90ead61f0d52d38a4849c
Onion <congma@outlook.com>
2024-04-27 22:15:18 +0800

    I don't give a shit about you

---
+++ src/201-300/234.palindrome-linked-list.test.ts
@@ -0,0 +1,64 @@
+import { ListNode } from '../dataStructure/ListNode';
+
+function isPalindrome( head: ListNode | null ): boolean {
+    const dummy = new ListNode( 0, head );
+    let fast = dummy, slow = dummy;
+
+    while( fast && fast.next ) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    let second = reverse( slow );
+    slow.next  = null;
+    let first = dummy.next;
+
+    while( second ) {
+        if( first.val !== second.val ) {
+            return false;
+        }
+        first = first.next;
+        second = second.next;
+    }
+
+    return true;
+
+
+    function reverse( node: ListNode | null ): ListNode | null {
+        const pre = node;
+        let cur = node.next;
+        pre.next = null;
+
+        while( cur ) {
+            const next = cur.next;
+            cur.next = pre.next;
+            pre.next = cur;
+            cur = next;
+        }
+
+        return pre.next;
+    }
+}
+
+describe( '234.palindrome-linked-list', () => {
+
+    it( '[1,2,2,1]', () => {
+        // slow = 2
+        const list = ListNode.of( [1,2,2,1] );
+        expect( isPalindrome( list ) ).toBeTruthy();
+    } );
+
+    it( '[1,2,3,2,1]', () => {
+        // slow = 3
+        const list = ListNode.of( [1,2,3,2,1] );
+        expect( isPalindrome( list ) ).toBeTruthy();
+    } );
+
+    it( '[1,2]', () => {
+        // slow = 1
+        const list = ListNode.of( [1,2] );
+        expect( isPalindrome( list ) ).toBeFalsy();
+    } );
+   
+} );
+
