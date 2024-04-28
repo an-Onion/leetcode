ac9501de6a242eb2d00341435cc9cba07a3ae8ce
Onion <congma@outlook.com>
2024-04-28 17:02:27 +0800

    another PTO day

--- test/236.lowest-common-ancestor-of-a-binary-tree.test.ts
+++ test/236.lowest-common-ancestor-of-a-binary-tree.test.ts
@@ -5,22 +5,22 @@ describe( 'Lowest Common Ancestor of a Binary Tree', () => {
 
   it( '[3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1', () => {
     const root = TreeNode.of( [3,5,1,6,2,0,8,undefined,undefined,7,4] );
-    const p = new TreeNode( 5 );
-    const q = new TreeNode( 1 );
+    const p = root.left;
+    const q = root.right;
     expect( lowestCommonAncestor( root, p, q ).val ).toBe( 3 );
   } );
 
   it( '[3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4', () => {
     const root = TreeNode.of( [3,5,1,6,2,0,8,undefined,undefined,7,4] );
-    const p = new TreeNode( 5 );
-    const q = new TreeNode( 4 );
+    const p = root.left; // 4
+    const q = root.left.right.right; // 4
     expect( lowestCommonAncestor( root, p, q ).val ).toBe( 5 );
   } );
 
   it( '[1,2], p = 1, q = 2', () => {
     const root = TreeNode.of( [1,2] );
-    const p = new TreeNode( 1 );
-    const q = new TreeNode( 2 );
+    const p = root;
+    const q = root.left;
     expect( lowestCommonAncestor( root, p, q ).val ).toBe( 1 );
   } );
 } );
