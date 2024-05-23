ca1117b4ed5cea1562ee501bc69610eca7e8654b
Onion <congma@outlook.com>
2024-05-23 23:27:19 +0800

    a stupid problem

---
+++ src/301-400/342.power-of-four.test.ts
@@ -0,0 +1,18 @@
+function isPowerOfFour( x: number ): boolean {
+
+    return x > 0 && ( x & x-1 ) == 0 && ( x&( 0x55555555 ) ) != 0;
+}
+
+describe( '342. power of four', () => {
+    it( '16', () => {
+        expect( isPowerOfFour( 16 ) ).toBeTruthy();
+    } );
+
+    it( '5', () => {
+        expect( isPowerOfFour( 5 ) ).toBeFalsy();
+    } );
+
+    it( '1', () => {
+        expect( isPowerOfFour( 1 ) ).toBeTruthy();
+    } );
+} );
