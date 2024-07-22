6d30b22d66b391f698293112073e7d7808c53f17
Onion <congma@outlook.com>
2024-07-22 21:46:59 +0800

    I wonder the solution has some bugs

---
+++ src/2000/2101.detonate-the-maximum-bombs.test.ts
@@ -0,0 +1,46 @@
+function maximumDetonation( bombs: number[][] ): number {
+    const N = bombs.length;
+    const graph: Array<Set<number>> = Array.from( { length: N },() => new Set<number>() );
+
+    for ( let i = 0; i < N; i++ ) {
+        const [xi, yi, ri] = bombs[i];
+
+        for ( let j = 0; j < N; ++j ) {
+            const [xj, yj] = bombs[j];
+            if ( ( xi - xj ) ** 2 + ( yi - yj ) ** 2 <= ri ** 2 ) {
+                graph[i].add( j );
+            }
+        }
+    }
+
+    for ( let i = 0; i < N; i++ ) {
+        const adj = graph[i];
+        for ( const u of adj ) {
+            for ( const v of graph[u] ) {
+                adj.add( v );
+            }
+        }
+    }
+    let ans = 0;
+
+    for( const set of graph ) {
+        ans = Math.max( ans, set.size );
+    }
+    return ans;
+}
+
+describe( '2101. Detonate the Maximum Bombs', () => {
+    it( 'example 1', () => {
+        expect( maximumDetonation( [[2,1,3],[6,1,4]] ) ).toBe( 2 );
+    } );
+
+
+    it( 'example 2', () => {
+        expect( maximumDetonation( [[1,1,5],[10,10,5]] ) ).toBe( 1 );
+    } );
+
+    it( 'example 3', () => {
+        expect( maximumDetonation( [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]] ) ).toBe( 5 );
+    } );
+
+} );
