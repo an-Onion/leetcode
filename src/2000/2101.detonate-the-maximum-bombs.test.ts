08e4d51a0ef0f8c56bd7fa223f6ce5fa8109e0ed
Onion <congma@outlook.com>
2024-07-22 21:52:44 +0800

    fix the bug of previous solution

--- src/2000/2101.detonate-the-maximum-bombs.test.ts
+++ src/2000/2101.detonate-the-maximum-bombs.test.ts
@@ -1,6 +1,9 @@
 function maximumDetonation( bombs: number[][] ): number {
     const N = bombs.length;
-    const graph: Array<Set<number>> = Array.from( { length: N },() => new Set<number>() );
+    const graph: Array<Set<number>> = Array.from(
+        { length: N },
+        () => new Set<number>(),
+    );
 
     for ( let i = 0; i < N; i++ ) {
         const [xi, yi, ri] = bombs[i];
@@ -13,17 +16,17 @@ function maximumDetonation( bombs: number[][] ): number {
         }
     }
 
-    for ( let i = 0; i < N; i++ ) {
-        const adj = graph[i];
-        for ( const u of adj ) {
-            for ( const v of graph[u] ) {
-                adj.add( v );
-            }
+    for ( let v = 0; v < N; v++ ) {
+        const dest = graph[v];
+        for ( let u = 0; u < N; u++ ) {
+            if ( !graph[u].has( v ) ) continue;
+
+            dest.forEach( ( w ) => graph[u].add( w ) );
         }
     }
     let ans = 0;
 
-    for( const set of graph ) {
+    for ( const set of graph ) {
         ans = Math.max( ans, set.size );
     }
     return ans;
@@ -31,16 +34,32 @@ function maximumDetonation( bombs: number[][] ): number {
 
 describe( '2101. Detonate the Maximum Bombs', () => {
     it( 'example 1', () => {
-        expect( maximumDetonation( [[2,1,3],[6,1,4]] ) ).toBe( 2 );
+        expect(
+            maximumDetonation( [
+                [2, 1, 3],
+                [6, 1, 4],
+            ] ),
+        ).toBe( 2 );
     } );
 
-
     it( 'example 2', () => {
-        expect( maximumDetonation( [[1,1,5],[10,10,5]] ) ).toBe( 1 );
+        expect(
+            maximumDetonation( [
+                [1, 1, 5],
+                [10, 10, 5],
+            ] ),
+        ).toBe( 1 );
     } );
 
     it( 'example 3', () => {
-        expect( maximumDetonation( [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]] ) ).toBe( 5 );
+        expect(
+            maximumDetonation( [
+                [1, 2, 3],
+                [2, 3, 1],
+                [3, 4, 2],
+                [4, 5, 3],
+                [5, 6, 4],
+            ] ),
+        ).toBe( 5 );
     } );
-
 } );
