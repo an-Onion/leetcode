23dd5fc59d4f0d55233ac6c34594e139a58b1ee1
Onion <congma@outlook.com>
2024-10-11 22:58:51 +0800

    The same as yesterday

--- src/3000/3162.find-the-number-of-good-pairs-i.test.ts
+++ src/3000/3162.find-the-number-of-good-pairs-i.test.ts
@@ -1,29 +1,20 @@
-function numberOfPairs( nums1: number[], nums2: number[], k: number ): number {
-    const pairs = new Map<number, number>();
-    for ( const num of nums2 ) {
-        pairs.set( num, ( pairs.get( num ) ?? 0 ) + 1 );
-    }
-
-    let count = 0;
-    for ( const num of nums1 ) {
-        if ( num % k !== 0 ) continue;
-        const target = num / k;
-        for ( let div = 1; div * div <= target; div++ ) {
-            if ( target % div !== 0 ) continue;
-
-            if ( pairs.has( div ) ) {
-                count += pairs.get( div );
-            }
-
-            if ( div * div !== target && pairs.has( target / div ) ) {
-                count += pairs.get( target / div );
+export function numberOfPairs(
+    nums1: number[],
+    nums2: number[],
+    k: number,
+): number {
+    let ret = 0;
+    for ( const a of nums1 ) {
+        for ( const b of nums2 ) {
+            if ( a % ( b * k ) === 0 ) {
+                ret++;
             }
         }
     }
-    return count;
+    return ret;
 }
 
-describe( '3162. Find the Number of Good Pairs', () => {
+describe( '3162. Find the Number of Good Pairs I', () => {
     it( 'e.g. 1', () => {
         const nums1 = [1, 3, 4],
             nums2 = [1, 3, 4],
