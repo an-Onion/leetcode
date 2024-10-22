c5fb76325880d932d9e0ffafd4e1f68dc77a71d1
Onion <congma@outlook.com>
2024-10-22 21:47:50 +0800

    Too many meeting today

---
+++ src/3000/3184.count-pairs-that-form-a-complete-day-i.test.ts
@@ -0,0 +1,23 @@
+function countCompleteDayPairs( hours: number[] ): number {
+    const map = new Map<number, number>();
+    let ret = 0;
+    for ( const hour of hours ) {
+        const quotient = hour % 24;
+        ret += map.get( ( 24 - quotient ) % 24 ) ?? 0;
+        const count = ( map.get( quotient ) ?? 0 ) + 1;
+        map.set( quotient, count );
+    }
+    return ret;
+}
+
+describe( '3184. Count Pairs That Form a Complete Day I', () => {
+    it( 'e.g. 1', () => {
+        const hours = [12, 12, 30, 24, 24];
+        expect( countCompleteDayPairs( hours ) ).toBe( 2 );
+    } );
+
+    it( 'e.g. 2', () => {
+        const hours = [72, 48, 24, 3];
+        expect( countCompleteDayPairs( hours ) ).toBe( 3 );
+    } );
+} );
