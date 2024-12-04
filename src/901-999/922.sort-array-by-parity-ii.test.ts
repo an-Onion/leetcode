caf22282d3d117f500d721705b82aca766c1e0aa
Onion <congma@outlook.com>
2024-12-04 22:14:15 +0800

    2 pointer algorithm

---
+++ src/901-999/922.sort-array-by-parity-ii.test.ts
@@ -0,0 +1,31 @@
+function sortArrayByParityII( nums: number[] ): number[] {
+    let odd = 1;
+
+    for ( let even = 0; even < nums.length; even += 2 ) {
+        if ( nums[even] % 2 === 1 ) {
+            while ( nums[odd] % 2 === 1 ) {
+                odd += 2;
+            }
+            [nums[odd], nums[even]] = [nums[even], nums[odd]];
+        }
+    }
+    return nums;
+}
+
+describe( '922. Sort Array By Parity II', () => {
+    it( 'case 1', () => {
+        const nums = [4, 2, 5, 7];
+        const ret = sortArrayByParityII( nums );
+        expect( [
+            [4, 5, 2, 7],
+            [4, 7, 2, 5],
+            [2, 5, 4, 7],
+            [2, 7, 4, 5],
+        ] ).toContainEqual( ret );
+    } );
+
+    it( 'case 2', () => {
+        const nums = [2, 3];
+        expect( [[2, 3]] ).toContainEqual( sortArrayByParityII( nums ) );
+    } );
+} );
