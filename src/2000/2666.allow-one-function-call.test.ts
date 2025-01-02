6a9991feab1630f4f0d5edd4f911183e9b3e7b58
Onion <congma@outlook.com>
2025-01-02 21:59:14 +0800

    build trust

---
+++ src/2000/2666.allow-one-function-call.test.ts
@@ -0,0 +1,37 @@
+type JSONValue =
+    | null
+    | boolean
+    | number
+    | string
+    | JSONValue[]
+    | { [key: string]: JSONValue };
+type OnceFn = ( ...args: JSONValue[] ) => JSONValue | undefined;
+
+function once( fn: ( ...args: number[] ) => number ): OnceFn {
+    let hasBeenCalled = false;
+    return function ( ...args ) {
+        if ( !hasBeenCalled ) {
+            hasBeenCalled = true;
+            return fn.apply( this, args );
+        }
+    };
+}
+
+describe( '2666. allow one function call', () => {
+    it( 'case 1', () => {
+        const fn = ( a, b, c ) => a + b + c;
+        const onceFn = once( fn );
+
+        expect( onceFn( 1, 2, 3 ) ).toBe( 6 );
+        expect( onceFn( 2, 3, 6 ) ).toBeUndefined();
+    } );
+
+    it( 'case 2', () => {
+        const fn = ( a, b, c ) => a * b * c;
+        const onceFn = once( fn );
+
+        expect( onceFn( 5, 7, 4 ) ).toBe( 140 );
+        expect( onceFn( 2, 3, 6 ) ).toBeUndefined();
+        expect( onceFn( 4, 6, 8 ) ).toBeUndefined();
+    } );
+} );
