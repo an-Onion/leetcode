bfd1d4f1a46d0865068a2a7f06d83b8edfa4dbc1
Onion <congma@outlook.com>
2025-01-29 21:36:45 +0800

    Spring Festival

--- src/201-300/219.contains-duplicate-ii.test.ts
+++ src/201-300/219.contains-duplicate-ii.test.ts
@@ -1,29 +1,23 @@
 function containsNearbyDuplicate( nums: number[], k: number ): boolean {
     const map = new Map<number, number>();
-
-    for ( let i = 0; i < nums.length; i++ ) {
-
-        const prev = map.get( nums[i] ) ?? -Infinity;
-
-        if( i - prev <= k ) {
-            return true;
-        }
-        map.set( nums[i], i );
+    for ( let j = 0; j < nums.length; j++ ) {
+        const i = map.get( nums[j] );
+        if ( Math.abs( i - j ) <= k ) return true;
+        map.set( nums[j], j );
     }
     return false;
 }
 
 describe( '219. contains-duplicate-ii', () => {
     it( 'nums = [1,2,3,1], k = 3', () => {
-        expect( containsNearbyDuplicate( [1,2,3,1], 3 ) ).toBeTruthy( );
+        expect( containsNearbyDuplicate( [1, 2, 3, 1], 3 ) ).toBeTruthy();
     } );
 
     it( 'nums = [1,0,1,1], k = 1', () => {
-        expect( containsNearbyDuplicate( [1,0,1,1], 1 ) ).toBeTruthy( );
+        expect( containsNearbyDuplicate( [1, 0, 1, 1], 1 ) ).toBeTruthy();
     } );
 
     it( 'nums = [1,2,3,1,2,3], k = 2', () => {
-        expect( containsNearbyDuplicate( [1,2,3,1,2,3], 2 ) ).toBeFalsy( );
+        expect( containsNearbyDuplicate( [1, 2, 3, 1, 2, 3], 2 ) ).toBeFalsy();
     } );
-
 } );
