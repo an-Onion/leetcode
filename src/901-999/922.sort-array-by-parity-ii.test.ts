ab2510505060ae3dd09a8d4ec6217b0fabe1eddf
Onion <congma@outlook.com>
2025-02-04 21:54:47 +0800

    refactor 922

--- src/901-999/922.sort-array-by-parity-ii.test.ts
+++ src/901-999/922.sort-array-by-parity-ii.test.ts
@@ -2,13 +2,16 @@ function sortArrayByParityII( nums: number[] ): number[] {
     let odd = 1;
 
     for ( let even = 0; even < nums.length; even += 2 ) {
-        if ( nums[even] % 2 === 1 ) {
-            while ( nums[odd] % 2 === 1 ) {
-                odd += 2;
-            }
-            [nums[odd], nums[even]] = [nums[even], nums[odd]];
+        if ( nums[even] % 2 === 0 ) {
+            continue;
         }
+
+        while ( nums[odd] % 2 === 1 ) {
+            odd += 2;
+        }
+        [nums[even], nums[odd]] = [nums[odd], nums[even]];
     }
+
     return nums;
 }
 
