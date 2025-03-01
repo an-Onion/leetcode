e5745e0f826c633307adec0aea5e9f6f80f3a8f6
Onion <congma@outlook.com>
2025-03-01 20:57:42 +0800

    refactor 131

--- src/101-200/131.palindrome-partitioning.test.ts
+++ src/101-200/131.palindrome-partitioning.test.ts
@@ -1,43 +1,50 @@
 function partition( s: string ): string[][] {
+    const dp = Array.from( { length: s.length }, () =>
+        Array( s.length ).fill( false ),
+    );
 
-    const dp = Array.from( Array( s.length ), () => Array( s.length ).fill( false ) );
-
-    function DP ( i, j ){
-        if( i >= j ) return true;
-        return dp[i][j];
-    }
-
-    for( let len = 0; len < s.length; len++ ) {
-        for( let i = 0; i < s.length - len; i++ ) {
+    for ( let len = 0; len < s.length; len++ ) {
+        for ( let i = 0; i < s.length - len; i++ ) {
             const j = i + len;
-            if( s[i] !== s[j] ) continue;
+            if ( s[i] !== s[j] ) continue;
             dp[i][j] = DP( i + 1, j - 1 );
         }
     }
 
-    const ret: Array<string[]> = [];
+    return DFS( 0, [] );
 
-    function DFS( start: number, path: string[] ){
-        if( start === s.length ) {
-            return ret.push( [...path] );
+    function DFS( start: number, path: string[] ) {
+        if ( start === s.length ) {
+            return [[...path]];
         }
-        for( let i = start; i < s.length; i++ ) {
-            if( dp[start][i] ){
-                DFS( i + 1, [...path, s.substring( start, i + 1 )] );
-            }
+        const ret: Array<string[]> = [];
+        for ( let i = start; i < s.length; i++ ) {
+            if ( !dp[start][i] ) continue;
+            ret.push( ...DFS( i + 1, [...path, s.substring( start, i + 1 )] ) );
         }
+        return ret;
     }
-    DFS( 0, [] );
 
-    return ret;
+    function DP( i: number, j: number ) {
+        return i >= j ? true : dp[i][j];
+    }
 }
 
 describe( '131. partition', () => {
     it( 'aab', () => {
-        expect( partition( 'aab' ).sort() ).toEqual( [ [ 'aa', 'b' ], [ 'a', 'a', 'b' ] ].sort() );
+        expect( partition( 'aab' ).sort() ).toEqual(
+            [
+                ['aa', 'b'],
+                ['a', 'a', 'b'],
+            ].sort(),
+        );
+    } );
+
+    it( 'aa', () => {
+        expect( partition( 'aa' ) ).toEqual( [['a', 'a'], ['aa']] );
     } );
 
     it( 'a', () => {
-        expect( partition( 'a' ) ).toEqual( [ [ 'a' ] ] );
+        expect( partition( 'a' ) ).toEqual( [['a']] );
     } );
 } );
