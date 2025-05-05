0482cd7c44af7c267990900553747bf056d6ac15
Onion <congma@outlook.com>
2025-05-05 22:37:36 +0800

    Domino 4

---
+++ src/701-800/790.domino-and-tromino-tiling.test.ts
@@ -0,0 +1,20 @@
+function numTilings( n: number ): number {
+    const MOD: number = 1e9 + 7;
+
+    const dp = [1, 1, 2];
+    for ( let i = 3; i <= n; i++ ) {
+        const val = ( dp[i - 1] * 2 + dp[i - 3] ) % MOD;
+        dp.push( val );
+    }
+    return dp[n];
+}
+
+describe( 'Domino and Tromino Tiling', () => {
+    it( '3', () => {
+        expect( numTilings( 3 ) ).toBe( 5 );
+    } );
+
+    it( '1', () => {
+        expect( numTilings( 1 ) ).toBe( 1 );
+    } );
+} );
