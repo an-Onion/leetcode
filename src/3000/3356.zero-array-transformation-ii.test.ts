21bc1a7e629ddaf02b5b5928c19c002fc77cde00
Onion <congma@outlook.com>
2025-05-21 22:30:48 +0800

    differential array + 2 pointers

---
+++ src/3000/3356.zero-array-transformation-ii.test.ts
@@ -0,0 +1,43 @@
+function minZeroArray( nums: number[], queries: number[][] ): number {
+    const delta = Array( nums.length + 1 ).fill( 0 );
+
+    let k = 0,
+        sum = 0;
+
+    for ( let i = 0; i < nums.length; i++ ) {
+        sum += delta[i];
+        const num = nums[i];
+        while ( k < queries.length && sum < num ) {
+            const [l, r, val] = queries[k];
+            delta[l] += val;
+            delta[r + 1] -= val;
+            if ( l <= i && i <= r ) {
+                sum += val;
+            }
+            k++;
+        }
+        if ( sum < num ) return -1;
+    }
+    return k;
+}
+
+describe( '3356. minZeroArray', () => {
+    it( 'case 1', () => {
+        const nums = [2, 0, 2],
+            queries = [
+                [0, 2, 1],
+                [0, 2, 1],
+                [1, 1, 3],
+            ];
+        expect( minZeroArray( nums, queries ) ).toBe( 2 );
+    } );
+
+    it( 'case 2', () => {
+        const nums = [4, 3, 2, 1],
+            queries = [
+                [1, 3, 2],
+                [0, 2, 1],
+            ];
+        expect( minZeroArray( nums, queries ) ).toBe( -1 );
+    } );
+} );
