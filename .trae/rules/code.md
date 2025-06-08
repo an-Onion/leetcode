0d2f749306317107cf4b98f3e628e74b94550b85
Onion <congma@outlook.com>
2025-06-08 11:56:12 +0800

    add rules for trae

---
+++ .trae/rules/code.md
@@ -0,0 +1,7 @@
+# code rules
+
+## Avoid Using else {} Blocks
+
+-   Prefer **guard clauses** over else statements to reduce nested logic and improve code readability.
+
+-   In loop structures, use `continue` within if conditions to exit the current iteration early, avoiding deep nesting patterns.
--- src/301-400/386.lexicographical-numbers.test.ts
+++ src/301-400/386.lexicographical-numbers.test.ts
@@ -1,22 +1,25 @@
 function lexicalOrder( n: number ): number[] {
     const ret = [];
-    function DFS( prev: number ) {
-        if ( prev > n ) return;
-        ret.push( prev );
-        for ( let i = 0; i <= 9; i++ ) {
-            const next = prev * 10 + i;
-            if ( next === 0 ) continue;
-            DFS( next );
+    let number = 1;
+    for ( let i = 1; i <= n; i++ ) {
+        ret.push( number );
+        if ( number * 10 <= n ) {
+            number *= 10;
+            continue;
         }
+        while ( number % 10 === 9 || number + 1 > n ) {
+            number = Math.floor( number / 10 );
+        }
+        number++;
     }
-    DFS( 0 );
-    ret.shift();
     return ret;
 }
 
 describe( '386. lexicalOrder', () => {
     it( '13', () => {
-        expect( lexicalOrder( 13 ) ).toEqual( [1,10,11,12,13,2,3,4,5,6,7,8,9] );
+        expect( lexicalOrder( 13 ) ).toEqual( [
+            1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9,
+        ] );
     } );
 
     it( '2', () => {
