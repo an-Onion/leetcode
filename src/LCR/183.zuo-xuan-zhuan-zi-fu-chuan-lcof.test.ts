1a7ef7633ee538e4c59a057a969a69366a6c5889
Onion <congma@outlook.com>
2025-10-04 21:36:48 +0800

    Still can not access to github'

--- src/LCR/183.zuo-xuan-zhuan-zi-fu-chuan-lcof.test.ts
+++ src/LCR/183.zuo-xuan-zhuan-zi-fu-chuan-lcof.test.ts
@@ -2,7 +2,7 @@ function dynamicPassword( password: string, target: number ): string {
     return password.slice( target ) + password.slice( 0, target );
 }
 
-describe( '183. zuo-xuan-zhuan-zi-fu-chuan-lcof', () => {
+describe( 'LCR 183. zuo-xuan-zhuan-zi-fu-chuan-lcof', () => {
     it( 'case 1', () => {
         expect( dynamicPassword( 's3cur1tyC0d3', 4 ) ).toBe( 'r1tyC0d3s3cu' );
     } );
