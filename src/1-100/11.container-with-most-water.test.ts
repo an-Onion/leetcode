283b1b9ed5f7f7179a84a38b9e682929bcdcf26b
Onion <congma@outlook.com>
2025-10-05 21:32:21 +0800

    From DFS to BFS

--- src/1-100/11.container-with-most-water.test.ts
+++ src/1-100/11.container-with-most-water.test.ts
@@ -1,24 +1,24 @@
-function maxArea( height: number[] ): number {
+function maxArea(height: number[]): number {
     let [l, r] = [0, height.length - 1];
 
     let sofar = 0;
-    while ( l < r ) {
-        const area = Math.min( height[l], height[r] ) * ( r - l );
-        sofar = Math.max( sofar, area );
-        if ( height[l] < height[r] ) l++;
-        else if ( height[l] === height[r] ) r--, l++;
+    while (l < r) {
+        const area = Math.min(height[l], height[r]) * (r - l);
+        sofar = Math.max(sofar, area);
+        if (height[l] < height[r]) l++;
+        else if (height[l] === height[r]) r--, l++;
         else r--;
     }
 
     return sofar;
 }
 
-describe( 'Container With Most Water', () => {
-    it( '[1,1]', () => {
-        expect( maxArea( [1, 1] ) ).toBe( 1 );
-    } );
+describe('11. Container With Most Water', () => {
+    it('[1,1]', () => {
+        expect(maxArea([1, 1])).toBe(1);
+    });
 
-    it( '[4,3,2,1,4]', () => {
-        expect( maxArea( [4, 3, 2, 1, 4] ) ).toBe( 16 );
-    } );
-} );
+    it('[4,3,2,1,4]', () => {
+        expect(maxArea([4, 3, 2, 1, 4])).toBe(16);
+    });
+});
--- src/401-500/417.pacific-atlantic-water-flow.test.ts
+++ src/401-500/417.pacific-atlantic-water-flow.test.ts
@@ -1,44 +1,67 @@
-function pacificAtlantic( heights: number[][] ): number[][] {
-    const M = heights.length,
-        N = heights[0].length;
-    const pacific = Array.from( { length: M }, () => Array( N ).fill( 0 ) );
+import { Queue } from '@datastructures-js/queue';
 
-    for ( let i = 0; i < M; i++ ) {
-        DFS( i, 0, 1 );
-        DFS( i, N - 1, 2 );
+function pacificAtlantic(heights: number[][]): number[][] {
+    const [m, n] = [heights.length, heights[0].length];
+
+    const pacific = Array.from({ length: m }, () => Array(n).fill(false));
+    const atlantic = Array.from({ length: m }, () => Array(n).fill(false));
+
+    const dirs = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+    ];
+
+    for (let i = 0; i < m; i++) {
+        BFS(i, 0, pacific);
+        BFS(i, n - 1, atlantic);
     }
 
-    for ( let j = 0; j < N; j++ ) {
-        DFS( 0, j, 1 );
-        DFS( M - 1, j, 2 );
+    for (let j = 0; j < n; j++) {
+        BFS(0, j, pacific);
+        BFS(m - 1, j, atlantic);
     }
 
-    const result: number[][] = [];
+    const ret: number[][] = [];
 
-    for ( let i = 0; i < M; i++ ) {
-        for ( let j = 0; j < N; j++ ) {
-            if ( pacific[i][j] === 3 ) {
-                result.push( [i, j] );
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (!pacific[i][j] || !atlantic[i][j]) continue;
+            ret.push([i, j]);
+        }
+    }
+    return ret;
+
+    function BFS(row: number, col: number, ocean: boolean[][]) {
+        if (ocean[row][col]) return;
+
+        const Q = new Queue<[number, number]>([[row, col]]);
+        ocean[row][col] = true;
+
+        while (Q.size()) {
+            const [row, col] = Q.dequeue();
+            for (const [dr, dc] of dirs) {
+                const r = row + dr,
+                    c = col + dc;
+                if (
+                    r < 0 ||
+                    r >= heights.length ||
+                    c < 0 ||
+                    c >= heights[0].length
+                )
+                    continue;
+                if (ocean[r][c]) continue;
+                if (heights[r][c] < heights[row][col]) continue;
+                ocean[r][c] = true;
+                Q.push([r, c]);
             }
         }
     }
-
-    return result;
-
-    function DFS( i: number, j: number, water: number ) {
-        if ( i < 0 || j < 0 || i >= M || j >= N ) return;
-        if ( ( water | pacific[i][j] ) === pacific[i][j] ) return;
-
-        pacific[i][j] |= water;
-        if ( heights[i][j] <= heights[i + 1]?.[j] ) DFS( i + 1, j, water );
-        if ( heights[i][j] <= heights[i - 1]?.[j] ) DFS( i - 1, j, water );
-        if ( heights[i][j] <= heights[i]?.[j + 1] ) DFS( i, j + 1, water );
-        if ( heights[i][j] <= heights[i]?.[j - 1] ) DFS( i, j - 1, water );
-    }
 }
 
-describe( '417. Pacific Atlantic Water Flow', () => {
-    it( 'e.g. 1', () => {
+describe('417. Pacific Atlantic Water Flow', () => {
+    it('case 1', () => {
         const heights = [
             [1, 2, 2, 3, 5],
             [3, 2, 3, 4, 4],
@@ -46,7 +69,7 @@ describe( '417. Pacific Atlantic Water Flow', () => {
             [6, 7, 1, 4, 5],
             [5, 1, 1, 2, 4],
         ];
-        expect( pacificAtlantic( heights ) ).toEqual( [
+        expect(pacificAtlantic(heights)).toEqual([
             [0, 4],
             [1, 3],
             [1, 4],
@@ -54,11 +77,24 @@ describe( '417. Pacific Atlantic Water Flow', () => {
             [3, 0],
             [3, 1],
             [4, 0],
-        ] );
-    } );
+        ]);
+    });
 
-    it( 'e.g. 2', () => {
+    it('case 2', () => {
+        const heights = [
+            [2, 1],
+            [1, 2],
+        ];
+        expect(pacificAtlantic(heights)).toEqual([
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1],
+        ]);
+    });
+
+    it('case 3', () => {
         const heights = [[1]];
-        expect( pacificAtlantic( heights ) ).toEqual( [[0, 0]] );
-    } );
-} );
+        expect(pacificAtlantic(heights)).toEqual([[0, 0]]);
+    });
+});
