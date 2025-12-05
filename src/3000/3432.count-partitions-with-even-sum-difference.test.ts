1220748ab449c61b1d3cffddef95e380b3924c40
Onion <congma@outlook.com>
2025-12-05 20:50:58 +0800

    Upgrade ts

--- src/3000/3432.count-partitions-with-even-sum-difference.test.ts
+++ src/3000/3432.count-partitions-with-even-sum-difference.test.ts
@@ -1,16 +1,12 @@
-import _ from 'lodash';
+import * as _ from 'lodash';
 
 function countPartitions( nums: number[] ): number {
-    const sum = _.sum( nums );
-    if ( sum % 2 !== 0 ) {
-        return 0;
-    }
-    return nums.length - 1;
+    return _.sum( nums ) % 2 === 0 ? nums.length - 1 : 0;
 }
 
 describe( '3432. count-partitions-with-even-sum-difference', () => {
     it( 'case 1', () => {
-        expect( countPartitions( [10, 10, 3, 7, 6] ) ).toBe( 0 );
+        expect( countPartitions( [10, 10, 3, 7, 6] ) ).toBe( 4 );
     } );
     it( 'case 2', () => {
         expect( countPartitions( [1, 2, 2] ) ).toBe( 0 );
