cecaf9a10ce9555fabb51021b1b1309f24b33c56
Onion <congma@outlook.com>
2026-01-02 15:00:06 +0800

    inclusion-exclusion principle

--- src/901-999/961.n-repeated-element-in-size-2n-array.test.ts
+++ src/901-999/961.n-repeated-element-in-size-2n-array.test.ts
@@ -1,13 +1,17 @@
 function repeatedNTimes( nums: number[] ): number {
     const n = nums.length;
 
-    while ( true ) {
-        const x = ( Math.random() * n ) | 0;
-        const y = ( Math.random() * n ) | 0;
-        if ( x !== y && nums[x] === nums[y] ) {
-            return nums[x];
+    const head = nums[0];
+    let prev = head;
+
+    for ( let i = 1; i < n; i++ ) {
+        if ( head === nums[i] ) {
+            return head;
         }
+        if ( nums[i] === prev ) return prev;
+        prev = nums[i];
     }
+    return prev;
 }
 
 describe( '961. N-Repeated Element in Size 2N Array', () => {
@@ -19,5 +23,9 @@ describe( '961. N-Repeated Element in Size 2N Array', () => {
     } );
     it( 'case 3', () => {
         expect( repeatedNTimes( [5, 1, 5, 2, 5, 3, 5, 4] ) ).toBe( 5 );
+    } );
+
+    it( 'case 4', () => {
+        expect( repeatedNTimes( [2, 0, 2, 2, 1, 2, 9, 7] ) ).toBe( 2 );
     } );
 } );
