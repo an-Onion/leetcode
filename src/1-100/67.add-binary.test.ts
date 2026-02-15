3eccc08dc5287418536ce490e6143b1b1b887495
Onion <congma@outlook.com>
2026-02-15 23:23:10 +0800

    too hard

---
+++ src/1-100/67.add-binary.test.ts
@@ -0,0 +1,21 @@
+function addBinary( a: string, b: string ): string {
+    let x = BigInt( `0b${a}` );
+    let y = BigInt( `0b${b}` );
+    while ( y ) {
+        const sum = x ^ y;
+        const carry = ( x & y ) << 1n;
+        x = sum;
+        y = carry;
+    }
+    return x.toString( 2 );
+}
+
+describe( 'Add Binary', () => {
+    it( '11 + 1', () => {
+        expect( addBinary( '11', '1' ) ).toBe( '100' );
+    } );
+
+    it( '1010 + 1011', () => {
+        expect( addBinary( '1010', '1011' ) ).toBe( '10101' );
+    } );
+} );
