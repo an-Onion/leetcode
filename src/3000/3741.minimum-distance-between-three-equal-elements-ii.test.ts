86654a33f6dd8c743d49f667a55535c6cc3c14c5
Onion <congma@outlook.com>
2026-04-11 16:51:17 +0800

    fix elint issue

--- src/3000/3741.minimum-distance-between-three-equal-elements-ii.test.ts
+++ src/3000/3741.minimum-distance-between-three-equal-elements-ii.test.ts
@@ -1,37 +1,37 @@
-function minimumDistance(nums: number[]): number {
-    const next = Array.from<number>({ length: nums.length }).fill(-1);
+function minimumDistance( nums: number[] ): number {
+    const next = Array.from<number>( { length: nums.length } ).fill( -1 );
     const occur = new Map<number, number>();
     let ans = nums.length + 1;
 
-    for (let i = nums.length - 1; i >= 0; i--) {
-        if (occur.has(nums[i])) {
-            next[i] = occur.get(nums[i])!;
+    for ( let i = nums.length - 1; i >= 0; i-- ) {
+        if ( occur.has( nums[i] ) ) {
+            next[i] = occur.get( nums[i] )!;
         }
-        occur.set(nums[i], i);
+        occur.set( nums[i], i );
     }
 
-    for (let i = 0; i < nums.length; i++) {
+    for ( let i = 0; i < nums.length; i++ ) {
         const secondPos = next[i];
         const thirdPos = next[secondPos];
-        if (secondPos !== -1 && thirdPos !== -1) {
-            ans = Math.min(ans, thirdPos - i);
+        if ( secondPos !== -1 && thirdPos !== -1 ) {
+            ans = Math.min( ans, thirdPos - i );
         }
     }
 
-    if (ans === nums.length + 1) {
+    if ( ans === nums.length + 1 ) {
         return -1;
     }
     return ans * 2;
 }
 
-describe('3741.minimum-distance-between-three-equal-elements-ii', () => {
-    it('case 1', () => {
-        expect(minimumDistance([1, 2, 1, 1, 3])).toBe(6);
-    });
-    it('case 2', () => {
-        expect(minimumDistance([1, 1, 2, 3, 2, 1, 2])).toBe(8);
-    });
-    it('case 3', () => {
-        expect(minimumDistance([1])).toBe(-1);
-    });
-});
+describe( '3741.minimum-distance-between-three-equal-elements-ii', () => {
+    it( 'case 1', () => {
+        expect( minimumDistance( [1, 2, 1, 1, 3] ) ).toBe( 6 );
+    } );
+    it( 'case 2', () => {
+        expect( minimumDistance( [1, 1, 2, 3, 2, 1, 2] ) ).toBe( 8 );
+    } );
+    it( 'case 3', () => {
+        expect( minimumDistance( [1] ) ).toBe( -1 );
+    } );
+} );
