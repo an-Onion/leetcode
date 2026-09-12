fd7a881d7ce4bc897a22819bfa66259e4e2f7a25
Onion <congma@outlook.com>
2026-09-13 00:05:23 +0800

    fix lint error

--- src/601-700/696.count-binary-substrings.test.ts
+++ src/601-700/696.count-binary-substrings.test.ts
@@ -1,29 +1,29 @@
-function countBinarySubstrings(s: string): number {
+function countBinarySubstrings( s: string ): number {
     const n = s.length;
     let ptr = 0,
         last = 0,
         ret = 0;
 
-    while (ptr < n) {
+    while ( ptr < n ) {
         const c = s[ptr];
         let count = 0;
-        while (ptr < n && s[ptr] === c) {
+        while ( ptr < n && s[ptr] === c ) {
             ptr++;
             count++;
         }
-        ret += Math.min(count, last);
+        ret += Math.min( count, last );
         last = count;
     }
 
     return ret;
 }
 
-describe('696. Count Binary Substrings', () => {
-    it('case 1', () => {
-        expect(countBinarySubstrings('00110011')).toBe(6);
-    });
+describe( '696. Count Binary Substrings', () => {
+    it( 'case 1', () => {
+        expect( countBinarySubstrings( '00110011' ) ).toBe( 6 );
+    } );
 
-    it('case 2', () => {
-        expect(countBinarySubstrings('10101')).toBe(4);
-    });
-});
+    it( 'case 2', () => {
+        expect( countBinarySubstrings( '10101' ) ).toBe( 4 );
+    } );
+} );
