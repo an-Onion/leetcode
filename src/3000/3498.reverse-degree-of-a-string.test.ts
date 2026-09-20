d02ad865ebc47be8574de3d5e67f6df4e8988381
Onion <congma@outlook.com>
2026-09-20 20:07:12 +0800

    refactor 3498

--- src/3000/3498.reverse-degree-of-a-string.test.ts
+++ src/3000/3498.reverse-degree-of-a-string.test.ts
@@ -1,16 +1,8 @@
 function reverseDegree( s: string ): number {
-    let ret = 0;
-
-    for ( let i = 0; i < s.length; i++ ) {
-        ret += ( i + 1 ) * getReversedIndex( s[i] );
-    }
-
-    return ret;
-
-    function getReversedIndex( c: string ) {
-        const v = c.charCodeAt( 0 ) - 96;
-        return 27 - v;
-    }
+    return s
+        .split( '' )
+        .map( ( c ) => 123 - c.charCodeAt( 0 ) )
+        .reduce( ( prev, cur, index ) => prev + cur * ( index + 1 ), 0 );
 }
 
 describe( '3498. reverse-degree-of-a-string', () => {
